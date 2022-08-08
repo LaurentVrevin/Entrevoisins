7f2b972248e44f4ff26692a72e48f7d8b18f68ea
Laurent Vrevin <laurent.vrevin@hotmail.fr>
2022-08-08 21:23:43 +0200

    Test unitaires ok, test instrumentés OK

--- app/src/androidTest/java/com/openclassrooms/entrevoisins/neighbour_list/NeighboursListTest.java
+++ app/src/androidTest/java/com/openclassrooms/entrevoisins/neighbour_list/NeighboursListTest.java
@@ -1,25 +1,6 @@
 
 package com.openclassrooms.entrevoisins.neighbour_list;
 
-import android.support.design.widget.FloatingActionButton;
-import android.support.test.espresso.ViewInteraction;
-import android.support.test.espresso.contrib.RecyclerViewActions;
-import android.support.test.espresso.matcher.ViewMatchers;
-import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
-import android.util.Log;
-import android.view.View;
-
-import com.openclassrooms.entrevoisins.R;
-import com.openclassrooms.entrevoisins.ui.neighbour_list.DetailNeighbourActivity;
-import com.openclassrooms.entrevoisins.ui.neighbour_list.ListNeighbourActivity;
-import com.openclassrooms.entrevoisins.utils.DeleteViewAction;
-
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
@@ -30,7 +11,24 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static com.openclassrooms.entrevoisins.utils.RecyclerViewItemCountAssertion.withItemCount;
 import static org.hamcrest.core.IsNull.notNullValue;
 
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.intent.Intents;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
+import com.openclassrooms.entrevoisins.R;
+import com.openclassrooms.entrevoisins.ui.neighbour_list.DetailNeighbourActivity;
+import com.openclassrooms.entrevoisins.ui.neighbour_list.ListNeighbourActivity;
+import com.openclassrooms.entrevoisins.utils.DeleteViewAction;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 
 /**
  * Test class for list of neighbours
@@ -40,7 +38,7 @@ public class NeighboursListTest {
 
     // This is fixed
     private static int ITEMS_COUNT = 12;
-    private static int FAVORITES_COUNT = 0;
+
 
     private ListNeighbourActivity mActivity;
 
@@ -49,11 +47,11 @@ public class NeighboursListTest {
             new ActivityTestRule(ListNeighbourActivity.class);
 
 
-
     @Before
     public void setUp() {
         mActivity = mActivityRule.getActivity();
         assertThat(mActivity, notNullValue());
+
     }
 
     /**
@@ -66,30 +64,27 @@ public class NeighboursListTest {
                 .check(matches(hasMinimumChildCount(1))); //vérifie s'il y a au moins 1 item
     }
 
+    @Test
+    public void myNeighbourgsDetailActivity_Openning() {
+        Intents.init();
+        //Pour que la page détail s'ouvre, nous devons générer un click sur une vue.
+        //Pour cela je dois d'abord afficher la liste. Ensuite je dois gérer le click dessus.
+        //onView(ViewMatchers.withId(R.id.list_neighbours)).check(withItemCount(ITEMS_COUNT));
+        //Je génère maintenant le click sur une des vues, comme la 2 par exemple
+        onView(ViewMatchers.withId(R.id.list_neighbours)).perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
+        //Vérifie que l'activité est bien lancée (je lance l'intent dans @before avec Intents.init(); afin d'éviter qu'elle pointe null
+        //Je la clôture avec la release.
+        intended(hasComponent(DetailNeighbourActivity.class.getName()));
+        Intents.release();
+    }
     /**
      * When we delete an item, the item is no more shown
      */
-    @Test
-    public void myNeighboursList_deleteAction_shouldRemoveItem() {
-        // Given : We remove the element at position 2
-        onView(ViewMatchers.withId(R.id.list_neighbours)).check(withItemCount(ITEMS_COUNT));
-        // When perform a click on a delete icon
-        onView(ViewMatchers.withId(R.id.list_neighbours))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(1, new DeleteViewAction()));
-        // Then : the number of element is 11
-        onView(ViewMatchers.withId(R.id.list_neighbours)).check(withItemCount(ITEMS_COUNT-1));
-    }
-    @Test
-    public void myNeighbourgsDetailActivity_Openning(){
-        //Pour que la page détail s'ouvre, nous devons générer un click sur une vue.
-        //Pour cela je dois d'abord afficher la liste. Ensuite je dois gérer le click dessus.
-        onView(ViewMatchers.withId(R.id.list_neighbours)).check(withItemCount(ITEMS_COUNT));
-        //Je génère maintenant le click sur une des vues, comme la 2 par exemple
-        onView(ViewMatchers.withId(R.id.list_neighbours)).perform(RecyclerViewActions.actionOnItemAtPosition(2, click()));
 
-    }
+
+
     @Test
-    public void myNeighbourgsDetailActivity_UserName_IsNotEmpty(){
+    public void myNeighbourgsDetailActivity_UserName_IsNotEmpty() {
         //Pour vérifier si le nom correspond bien au voisin sur lequel on a cliquer,
         // on procède comme pour la vérification de l'ouverture de la page détail activity
         // On doit ensuite vérifier si R.id.profilNameDetail est bien celui correspond, comme pour la position 2.
@@ -100,31 +95,45 @@ public class NeighboursListTest {
         onView(ViewMatchers.withId(R.id.list_neighbours)).perform(RecyclerViewActions.actionOnItemAtPosition(2, click()));
         //Je vérifie maintenant que le nom est bien "Chloé".
         onView(withId(R.id.profilNameDetail)).check(matches(withText("Chloé")));
+        //vérifie
     }
+
     @Test
-    public void myFavoritList_TabLayout_isFavorited (){
-        /**Pour vérifier que la liste des favoris comprend bien des favoris je dois :
+    public void myFavoritList_TabLayout_isFavorited() {
+        /*Pour vérifier que la liste des favoris comprend bien des favoris je dois :
          * Ouvrir la page Détail Activity
          * Générer le click sur le bouton Favori
          * Retourner en arrière sur le tabLayout
          * Switcher sur le tablayout favori
          * check qu'il y a bien le favori que j'ai ajouté.
-         * Nous allons prendre "Vincent" en index 3.
+         * Je vais prendre "Vincent" en index 3.
          */
-        //j'affiche la liste des favoris
-        onView(ViewMatchers.withId(R.id.list_neighbours)).check(withItemCount(ITEMS_COUNT));
+        //j'affiche la liste
+        //onView(ViewMatchers.withId(R.id.list_neighbours)).check(withItemCount(ITEMS_COUNT));
         //Je click sur Vincent pour l'ouvrir dans la Detail Activity
         onView(ViewMatchers.withId(R.id.list_neighbours)).perform(RecyclerViewActions.actionOnItemAtPosition(3, click()));
         //Je click sur le bouton favoris
-        onView(withId(R.id.floatingActionButtonDetail)).perform(click());
-
+        onView(ViewMatchers.withId(R.id.floatingActionButtonDetail)).perform(click());
         //Je retourne en arrière pour switcher sur la tab favori
         onView(ViewMatchers.withId(R.id.BackButton)).perform(click());
-        //sur le container, je dois passer sur le tab 2
-
-        onView(ViewMatchers.withId(R.id.list_favorite_neighbours)) //va rechercher le layout qui a pour id ListNeighbours
-                .check(matches(hasMinimumChildCount(1))); //vérifie s'il y a au moins 1 item
-
+        //je dois passer sur le tab 2 et ensuite check si Vincent s'y trouve bien
+        onView(ViewMatchers.withText("FAVORITES")).perform(click());
+        //onView(withId(R.id.list_favorite_neighbours)).check(new RecyclerViewItemCountAssertion(1));
+        onView(ViewMatchers.withId(R.id.list_favorite_neighbours)).check((matches(hasMinimumChildCount(1))));
+        /*onView(ViewMatchers.withId(R.id.list_favorite_neighbours)) //va rechercher le layout qui a pour id ListNeighbours
+         .check(matches(hasMinimumChildCount(1))); //vérifie s'il y a au moins 1 item
+         */
 
     }
+    @Test
+    public void myNeighboursList_deleteAction_shouldRemoveItem() {
+        // Given : We remove the element at position 2
+        onView(ViewMatchers.withId(R.id.list_neighbours)).check(withItemCount(ITEMS_COUNT));
+        // When perform a click on a delete icon
+        onView(ViewMatchers.withId(R.id.list_neighbours))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(2, new DeleteViewAction()));
+        // Then : the number of element is 11
+        onView(ViewMatchers.withId(R.id.list_neighbours)).check(withItemCount(ITEMS_COUNT - 1));
+    }
+
 }
